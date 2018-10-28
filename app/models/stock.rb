e9cbc0f11c82bce8846673ab59657b205e66a262
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-28 13:14:31 -0200

    feat: Adding associations between users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,16 @@
 #
 class Stock < ApplicationRecord
+
+  # Validations
   validates :ticker, presence: true
   validates :name, presence: true
   validates :last_price, presence: true
 
+  # Associations
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  # Class Methods
   def self.quote_request(stock_name)
     stock = StockQuote::Stock.quote(stock_name)
     new(ticker: stock.symbol, name: stock.company_name, last_price: stock.open)
