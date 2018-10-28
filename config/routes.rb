c0147df919ee8cbc108dc0dd3510d670c256d702
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-28 12:50:53 -0200

    feat: route for search stocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'my_portfolio', to: 'users#my_portfolio'
+  # Root
   root to: 'welcome#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # UsersController
+  get 'my_portfolio', to: 'users#my_portfolio'
+  # StockController
+  get 'search_stocks', to: 'stocks#search'
+
 end
